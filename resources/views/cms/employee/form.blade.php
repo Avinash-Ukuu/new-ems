4f324de171c4a25ac6d4631b755adab88b4d14a8
Avinash <avinash.ukuu@gmail.com>
2026-06-11 09:57:30 +0530

    employee form in progress

--- resources/views/cms/employee/form.blade.php
+++ resources/views/cms/employee/form.blade.php
@@ -30,7 +30,43 @@
                     </div>
 
                     <div class="row">
-                        
+                        <div class="form-group col-4">
+                            <label for="dob">DOB</label>
+                            <input type="date" class="form-control" id="dob" placeholder="Date Of Birth"
+                                name="dob" max="{{ date('Y-m-d') }}" value="{{ old('dob', $object->dob) }}">
+                            <small id="dob_error" class="text-danger"></small>
+                        </div>
+
+                        <div class="form-group col-4">
+                            <label for="gender">Gender</label>
+                            <select class="form-control" id="gender" name="gender">
+                                <option value="">-- Select Gender --</option>
+                                <option value="Male" {{ old('gender', $object->gender ?? '') == 'Male' ? 'selected' : '' }}>
+                                    Male
+                                </option>
+                                <option value="Female" {{ old('gender', $object->gender ?? '') == 'Female' ? 'selected' : '' }}>
+                                    Female
+                                </option>
+                                <option value="Other" {{ old('gender', $object->gender ?? '') == 'Other' ? 'selected' : '' }}>
+                                    Other
+                                </option>
+                            </select>
+                        </div>
+
+                        <div class="form-group col-4">
+                            <label for="address">Address</label>
+                            <input type="text" class="form-control" id="address" placeholder="Address"
+                                name="address" value="{{ old('address', $object->address) }}">
+                        </div>
+
+                    </div>
+
+                    <div class="row">
+                        <div class="form-group col-4">
+                            <label for="joining_date">Joining Date</label>
+                            <input type="date" class="form-control" id="joining_date" placeholder="Joining Date"
+                                name="joining_date" value="{{ old('joining_date', $object->joining_date) }}">
+                        </div>
                     </div>
 
                     <div class="form-group">
@@ -182,6 +218,35 @@
                 inputValue = inputValue.replace(/\D/g, '');
                 inputValue = inputValue.substring(0, 10);
                 $(this).val(inputValue);
+            });
+
+            $("#dob").on("change", function () {
+
+                let dob = new Date($(this).val());
+                let today = new Date();
+                today.setHours(0, 0, 0, 0);
+                if (dob > today) {
+                    $("#dob_error").text("Date of birth cannot be a future date.");
+                    $(this).val('');
+                    return;
+                }
+
+                let age = today.getFullYear() - dob.getFullYear();
+                let monthDiff = today.getMonth() - dob.getMonth();
+
+                if (
+                    monthDiff < 0 ||
+                    (monthDiff === 0 && today.getDate() < dob.getDate())
+                ) {
+                    age--;
+                }
+                if (age < 18) {
+                    $("#dob_error").text("Age must be at least 18 years.");
+                    $(this).val('');
+                    return;
+                }
+
+                $("#dob_error").text("");
             });
         });
     </script>
